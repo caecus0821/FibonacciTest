8aa15287b77ae59c05d0c60a1e83e0d6d28c9925
josepalo <jrppalo@gmail.com>
2021-03-03 12:22:49 +1100

    Initial Comit

---
+++ FibonacciTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main()
+        {
+
+            int num1 = 1;
+            int num2 = 0;
+            int num3 = 0;
+            string fib = "";
+            string fin = "0";
+            int counter = 1;
+            int range = 0;
+
+            Console.WriteLine("Enter Your fibonnaci Range: ");
+            range = Convert.ToInt32(Console.ReadLine());
+
+            while(counter < range)
+            {
+                fin += ", ";
+                num3 = num1 + num2;
+                fib = Convert.ToString(num3);
+                fin += fib;
+                counter++;
+                num1 = num2;
+                num2 = num3;
+            }
+            Console.WriteLine("Fibonacci numbers are: " + fin);
+
+        }
+
+    
+    }
+
